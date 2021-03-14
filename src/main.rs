f73613060b0aec8713526a49c9db35783ba42247
Blake Ridgway <blakeridgway@pm.me>
2021-03-14 03:43:54 -0500

    removed unused catch handlers

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,9 @@ use rocket::Request;
 use rocket::catchers;
 use rocket_contrib::serve::StaticFiles;
 
-#[catch(400)]
-fn bad_request() -> &'static str {
-    "Check your connection and settings, clear your cache, open a different browser, and retry."
-}
-
-#[catch(401)]
-fn auth_required() -> &'static str {
-    "You are trying to access a web page that requires a password."
-}
-#[catch(403)]
-fn forbidden() -> &'static str {
-    "Usually, getting this error means you have entered a URL or clicked a link that goes to a page that has been set up with access permissions, meaning you have to have an account or some other type of authorization in order to access the page."
-}
 #[catch(404)]
 fn not_found(req: &Request) -> String {
     format!("Oh no! We couldn't find the requested path '{}'", req.uri())
-}
-
-#[catch(405)]
-fn method_not_allowed() -> &'static str {
-    "Please refresh and try again"
 }
 
 #[catch(500)]
@@ -42,7 +24,7 @@ fn service_unavailable() -> &'static str {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .register(catchers![bad_request, auth_required, forbidden, method_not_allowed, not_found, internal_error, service_unavailable])
+        .register(catchers![not_found, internal_error, service_unavailable])
         .mount("/", StaticFiles::from("static"))
 }
 
